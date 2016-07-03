dd6bcca013ad0b40b1833de516ad6b89fdd47f0d
Sammy Franklin <sammyfranklin@yahoo.com>
2016-07-03 00:04:59 -0400

    First commit in test branch

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var Campground = require("../models/campground");
-var middleware = require("../middleware")
+var middleware = require("../middleware");
 
 //index
 router.get("/", function(req, res){
